1d3ad85da095781f35c148bd7c77688d7328b980
Grzegorz Godlewski <gg@gitgis.com>
2019-01-21 23:46:52 +0100

    first commit

---
+++ docs/js/searcher.js
@@ -0,0 +1,90 @@
+(function () {
+
+    var index;
+    var documents;
+
+    function loadIndex(indexUrl) {
+        if (index) {
+            return Promise.resolve(index);
+        }
+
+        return fetch(indexUrl)
+            .then(function (response) {
+                return response.json()
+                    .then(function (docs) {
+                        documents = docs
+                        // console.log(documents);
+
+                        index = lunr(function () {
+                            this.ref('uri');
+                            this.field('title');
+                            this.field('content');
+                            this.metadataWhitelist = ['position']
+
+                            documents.forEach(function (doc) {
+                                this.add(doc)
+                            }, this)
+                        });
+
+                        return index;
+                    })
+            })
+            .catch(function (err) {
+                console.error(err);
+            });
+    }
+
+    function performSearch(q, indexUrl, mainDir) {
+        loadIndex(indexUrl).then(function (index) {
+            var results = index.search(q);
+            results = results.map(function (result) {
+                var doc = documents.find(function (doc) {return doc.uri == result.ref});
+                return Object.assign({}, result, {
+                    title: doc ? doc.title : result.ref
+                })
+            });
+
+            console.log(results);
+
+            var toc = document.querySelector("body > main .docs-toc")
+            if (toc) toc.innerHTML = '';
+            var main = document.querySelector("body > main .docs-content");
+
+            // main.innerHTML = "<div class='search-results container-fluid'><div class='col-sm-3'></div>" +
+            //     "<div class='col'><h1>Search Results</h1><p></p><ul></ul></div></div>";
+            // main.innerHTML = "<div class='search-results row'><div class='col-sm-3 docs-left-column'></div>" +
+            //     "<div class='col docs-content'><h1>Search Results</h1><p></p><ul></ul></div></div>";
+            main.innerHTML = "<h1>Search Results</h1><p></p><ul></ul>";
+
+            results.forEach((function (result) {
+                var liElem = document.createElement("li");
+                var aElem = document.createElement("a");
+                liElem.appendChild(aElem);
+                aElem.href = mainDir+result.ref.replace(/\/_index$/, '/');
+                aElem.innerText = result.title;
+
+                main.querySelector("ul").appendChild(liElem);
+            }));
+
+            main.querySelector("p").innerHTML = "Found: "+(+results.length)+" results";
+        })
+    }
+
+    window.addEventListener("load", function() {
+        var mainDir = window.location.pathname.replace(/\/([^\/]+).*/, "/$1");
+        var forms = document.querySelectorAll(".search-form");
+        forms.forEach(function (form) {
+            form.addEventListener("submit", function () {
+                if (form.elements["q"].value) {
+                    performSearch(form.elements["q"].value, form.getAttribute("data-index"), mainDir)
+                }
+            });
+        })
+
+
+    });
+
+
+
+})();
+
